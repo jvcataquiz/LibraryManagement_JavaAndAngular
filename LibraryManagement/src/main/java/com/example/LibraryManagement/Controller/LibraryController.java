82e1a108c0de71269a07ac42f1f0197335cd8f5b
jvcataquiz <jvcataquiz@gmail.com>
2022-11-05 19:41:19 +0800

    created a handler to view specific data using the id

--- LibraryManagement/src/main/java/com/example/LibraryManagement/Controller/LibraryController.java
+++ LibraryManagement/src/main/java/com/example/LibraryManagement/Controller/LibraryController.java
@@ -3,9 +3,12 @@ package com.example.LibraryManagement.Controller;
 import com.example.LibraryManagement.Model.Library;
 import com.example.LibraryManagement.Repository.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/library/management")
@@ -19,6 +22,12 @@ public class LibraryController {
     @PostMapping("/list")
     public Library createNewBookRecord(@RequestBody Library library){
         return libraryRepository.save(library);
+    }
+    @GetMapping("/list/{id}")
+    public ResponseEntity<Library> getLibraryDetails(@PathVariable Long id){
+        Library library = libraryRepository.findById(id).get();
+
+        return ResponseEntity.ok(library);
 
     }
 
